c2dec138af2fc20ffec7c8c4a91f097fbf098f74
MKEALANI <2005ldybug@gmail.com>
2023-11-16 22:28:11 -0800

    Created the Navbar and made it look pretty. YAY \(^w^)/

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white text-black
-       dark:bg-[#090908] dark:text-white h-full selection:bg-gray-400
-       dark:selection:bg-gray-800`}
+       dark:bg-[#090908] dark:text-white h-full selection:bg-purple-400
+       dark:selection:bg-purple-400`}
        > 
        <Navbar/>
         {children}
