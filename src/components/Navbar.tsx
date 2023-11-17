9f9183c35c6f73d53856414a8f0f64757e13a472
MKEALANI <2005ldybug@gmail.com>
2023-11-16 17:23:54 -0800

    Fixed all the errors in the scripts. Also managed to implement a dark/light mode appearance. Was able to start hte layout and the Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,27 @@
 "use client";
 
+import { Disclosure } from "@headlessui/react";
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <Disclosure as="nav">
             {({open}) => (
                 <>
                 <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                   <div className="flex justify-between h-16">
-                        <div className="flex items-center">
+                    <div className="Flex justify-between h-16">
+                        <div className="Flex justify-between w-full">
                             <Link href="/">
                                 <h1 className="text-2xl font-medium">
-                                    Jan <span className="text-teal-500">Marshal</span>
+                                    LeeAnn <span className="text-teal-500">Nakazawa</span>
                                 </h1>
-                           
+                            
                             </Link>
-                        
                         </div>
-                     
-                     </div>                  
-                 </div>
+                    </div>
+                </div>
                 </>
-           )}
-    </Disclosure>
-   );
+            )}
+        </Disclosure>
+    )
 }
-   
